0e910a0074ddd451a7a5ecf446801007664eaa83
sumant1512 <sumantmishra511@gmail.com>
2025-12-14 21:57:21 +0530

    moved config and database file

---
+++ app/core/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True  # important
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def check_db_connection():
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        print("✅ Database connection successful")
+    except Exception as e:
+        print("❌ Database connection failed:", e)
+        raise
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.database import check_db_connection
+from app.core.database import check_db_connection
+# from app.routers import auth, super_admin, agency_admin, ecommerce, product
 
 app = FastAPI(title="MSuite API")
 
@@ -11,3 +12,10 @@ def startup():
 @app.get("/")
 def root():
     return {"status": "Server is running"}
+
+# app.include_router(auth.router, prefix="/auth")
+# app.include_router(super_admin.router, prefix="/super-admin")
+# app.include_router(agency_admin.router, prefix="/agency")
+# app.include_router(ecommerce.router, prefix="/ecommerce")
+# app.include_router(product.router, prefix="/products")
+
